bbf606a8d4487ff78234f1316bc573efff0793e0
user <user@gmail.com>
2023-01-08 15:03:09 +0600

    done

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(express.json());
 // Routes
 app.use("/home", home);
 
+app.use(express.static('frontend/builded'))
+app.get('*',(req, res)=>{
+	res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
